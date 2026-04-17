2bc2ce60c46024d89bce364d87d2d4ce3378ed4d
BhoneMyatOo17 <bhonemyatoo73@gmail.com>
2026-04-17 13:42:55 +0700

    Design componenets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,21 +25,18 @@
         @if (Route::has('login'))
             <nav class="flex items-center justify-end gap-4">
                 @auth
-                    <a href="{{ url('/dashboard') }}"
-                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                    <x-nav-link href="{{ url('/dashboard') }}" class="button">
                         Dashboard
-                    </a>
+                    </x-nav-link>
                 @else
-                    <a href="{{ route('login') }}"
-                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal">
+                    <x-nav-link href="{{ route('login') }}" class="primarybtn">
                         Log in
-                    </a>
+                    </x-nav-link>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                        <x-nav-link href="{{ route('register') }}" class="primarybtn">
                             Register
-                        </a>
+                        </x-nav-link>
                     @endif
                 @endauth
             </nav>
